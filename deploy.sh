e1bd66f05c9ebaf191fc8c47b158af99992b5d32
Azeria <azeriaketturenard@gmail.com>
2024-11-10 17:26:50 +0100

    deploy normalization: testing

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Relative route for cron execution
+cd "$(dirname "$0")"
+
 # Look for modified stuff
 printf "\033[0;34mDeploy\033[0m | Looking for git status...\n"
 if [[ $(git status --porcelain) ]]; then
     printf "\033[0;33mDeploy\033[0m | You have git stuff to do. Fix before deploying.\n"
-    exit 1
+#    exit 1
 fi
 printf "\033[0;32mDeploy\033[0m | Everything looks up to date.\n"
 
@@ -16,14 +19,14 @@ PULL_OUTPUT=$(git pull origin main)
 
 # Verificar si hubo cambios
 if [[ "$PULL_OUTPUT" != *"Already up to date."* ]]; then
-    printf "\033[0;33mDeploy\033[0m | There is new stuff. Execution continues."
+    printf "\033[0;33mDeploy\033[0m | There is new stuff. Execution continues.\n"
 fi
 
 if [ $? -ne 0 ]; then
     printf "\033[0;31mDeploy\033[0m | Error while updating. You f* up something again.\n"
     exit 1
 fi
-printf "\033[0;32mDeploy\033[0m |Applied origin changes, if any.\n"
+printf "\033[0;32mDeploy\033[0m | Applied origin changes, if any.\n"
 
 printf "
 
@@ -38,14 +41,14 @@ Executing year-left, a BlueSky bot to know
 how much time is left in the current year.\n
 "
 
-printf "\033[0;32mDeploy\033[0m | Loading .env variables..."
-export $(grep -v '^#' ../utils/year-left/.env | xargs)
+printf "\033[0;34mDeploy\033[0m | Loading .env variables...\n"
+#export $(grep -v '^#' ../utils/year-left/.env | xargs)
 
-printf "\033[0;32mDeploy\033[0m | Installing dependencies..."
-npm install
+printf "\033[0;34mDeploy\033[0m | Installing dependencies...\n"
+#npm install
 
-printf "\033[0;32mDeploy\033[0m | Compiling TypeScript..."
-npx tsc
+printf "\033[0;34mDeploy\033[0m | Compiling TypeScript...\n"
+#npx tsc
 
-printf "\033[0;32mDeploy\033[0m | Starting the service..."
-npm start
+printf "\033[0;34mDeploy\033[0m | Starting the service...\n"
+#npm start
