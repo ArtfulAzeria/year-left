9327a2012194eb31aab15a32cf85e13d84e8f44c
Azeria <azeriaketturenard@gmail.com>
2024-11-10 03:58:02 +0100

    KISS my #!bin/bash

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Look for modified stuff
+printf "🔵  | Looking for git status...\n"
+if [[ $(git status --porcelain) ]]; then
+    printf "🟡  | You have git stuff to do. Fix before deploying.\n"
+    exit 1
+fi
+printf "🟢  |\033[0;32mEverything looks up to date.\033[0m\n"
+
+printf "🔵  | Updating from origin...\n"
+# Pull origin
+git fetch origin
+git checkout main
+git pull origin main
+
+if [ $? -ne 0 ]; then
+    printf "🔴  | \033[0;31mError while updating. You f* up something again.\n"
+    exit 1
+fi
+printf "🟢  |\033[0;32mApplied origin changes, if any.\033[0m\n"
+
+printf "
+
+ █████╗ ███████╗███████╗██████╗ ██╗ █████╗
+██╔══██╗╚══███╔╝██╔════╝██╔══██╗██║██╔══██╗
+███████║  ███╔╝ █████╗  ██████╔╝██║███████║
+██╔══██║ ███╔╝  ██╔══╝  ██╔══██╗██║██╔══██║
+██║  ██║███████╗███████╗██║  ██║██║██║  ██║
+╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝
+
+Executing year-left, a BlueSky bot to know
+how much time is left in the current year.\n
+"
+
+printf "🔵  | Installing dependencies..."
+npm install
+
+printf "🔵  | Compiling TypeScript..."
+npx tsc
+
+printf "🔵  | Starting the service..."
+npm start
