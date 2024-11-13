5f429fd8b0bb0e46cc2173f2ef70029eb4e71bc5
Azeria <azeriaketturenard@gmail.com>
2024-11-13 06:10:34 +0100

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,56 +1,123 @@
 #!/bin/bash
 
-# Relative route for cron execution
+# Initialization
 cd "$(dirname "$0")"
+domain="bot.azeria.dev"
+source ../utils/generic/formats.sh
+error=false
 
-# Look for modified stuff
-printf "\033[0;34mDeploy\033[0m | Looking for git status...\n"
-if [[ $(git status --porcelain) ]]; then
-    printf "\033[0;33mDeploy\033[0m | You have git stuff to do. Fix before deploying.\n"
+# Default values for the flags
+mode="dev"
+account="none"
+git="ignore"
+log="info"
+
+# Function to display usage information
+function usage {
+    echo "Usage: $0 [options]"
+    echo ""
+    echo "Options:"
+    echo "  -h, --help                Show this help message and exit"
+    echo "  -m, --mode <mode>         Set the mode (default: dev, options: dev, pro)"
+    echo "  -a, --account <account>   Specify the account name (default: none)"
+    echo "  -g, --git <action>        Git action (default: ignore, options: ignore, update)"
+    echo "  -l, --log <level>         Log level (default: info, options: info, debug)"
+    exit 0
+}
+
+# Function to perform git update if requested
+function git_update {
+    echo -e "year-left ${LOG_INFO} Checking for updates in Origin..."
+    git fetch origin
+    local updates=$(git diff origin/main)
+    
+    if [[ -n "$updates" ]]; then
+        echo -e "year-left ${LOG_ERRR} Updates detected in the repository. Exiting..."
+        exit 1
+    else
+        echo -e "year-left ${LOG_SCES} No updates found. Proceeding..."
+    fi
+}
+
+# Parse options
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -h|--help)
+        usage
+        ;;
+    -m|--mode)
+        if [ -n "$2" ] && [[ "$2" != -* ]]; then
+            mode="$2"
+            if [[ "$mode" != "dev" && "$mode" != "pro" ]]; then
+                echo -e "year-left ${LOG_ERRR} Error: Invalid mode. Use 'dev' or 'pro'."
+                error=true
+        fi
+        else
+            echo -e "year-left ${LOG_ERRR} Error: Missing argument for -m|--mode."
+            error=true
+        fi
+        ;;
+    -a|--account)
+        if [ -n "$2" ] && [[ "$2" != -* ]]; then
+            account="$2"
+        else
+            echo -e "year-left ${LOG_ERRR} Error: Missing argument for -a|--account."
+            error=true
+        fi
+        ;;
+    -g|--git)
+        if [ -n "$2" ] && [[ "$2" != -* ]]; then
+            git="$2"
+            if [[ "$git" != "ignore" && "$git" != "update" ]]; then
+                echo -e "year-left ${LOG_ERRR} Error: Invalid git option. Use 'ignore' or 'update'."
+                error=true
+            fi
+        else
+            echo -e "year-left ${LOG_ERRR} Error: Missing argument for -g|--git."
+            error=true
+        fi
+        ;;
+    -l|--log)
+        if [ -n "$2" ] && [[ "$2" != -* ]]; then
+            log="$2"
+        else
+            echo -e "year-left ${LOG_ERRR} Error: Missing argument for -l|--log."
+            error=true
+        fi
+        ;;
+    *)
+        echo -e "year-left ${LOG_ERRR} Error: Unrecognized option '$1'"
+        error=true
+        ;;
+    esac
+    shift 1
+    shift 1
+done
+
+if $error; then
+    usage
     exit 1
 fi
-printf "\033[0;32mDeploy\033[0m | Everything looks up to date.\n"
 
-# Pull origin
-printf "\033[0;34mDeploy\033[0m | Updating from origin...\n"
-git fetch origin
-git checkout main
-PULL_OUTPUT=$(git pull origin main)
-
-# Look for changes from origin
-if [[ "$PULL_OUTPUT" != *"Already up to date."* ]]; then
-    printf "\033[0;33mDeploy\033[0m | There is new stuff.\n"
-    # Uncomment this to stop in case of changes
-    #exit 1
+if [[ "$log" == "debug" ]]; then
+    echo -e "year-left ${LOG_DBUG} Mode: $mode"
+    echo -e "year-left ${LOG_DBUG} Account: $account"
+    echo -e "year-left ${LOG_DBUG} Git action: $git"
+    echo -e "year-left ${LOG_DBUG} Log level: $log"
 fi
 
-if [ $? -ne 0 ]; then
-    printf "\033[0;31mDeploy\033[0m | Error while updating. You f* up something again.\n"
-    exit 1
+# Handle git update if the option is set to "update"
+if [[ "$git" == "update" ]]; then
+    git_update
 fi
-printf "\033[0;32mDeploy\033[0m | Applied origin changes, if any.\n"
 
-printf "
+# Example logic based on the mode
+if [[ "$mode" == "dev" ]]; then
+    echo -e "year-left ${LOG_INFO} Running in development mode (dry run)."
+else
+    echo -e "year-left ${LOG_INFO} Running in production mode (posting live)."
+fi
 
- █████╗ ███████╗███████╗██████╗ ██╗ █████╗
-██╔══██╗╚══███╔╝██╔════╝██╔══██╗██║██╔══██╗
-███████║  ███╔╝ █████╗  ██████╔╝██║███████║
-██╔══██║ ███╔╝  ██╔══╝  ██╔══██╗██║██╔══██║
-██║  ██║███████╗███████╗██║  ██║██║██║  ██║
-╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝
 
-Executing year-left, a BlueSky bot to know
-how much time is left in the current year.\n
-"
 
-printf "\033[0;34mDeploy\033[0m | Loading .env variables...\n"
-export $(grep -v '^#' ../utils/year-left/.env | xargs)
 
-printf "\033[0;34mDeploy\033[0m | Installing dependencies...\n"
-npm install
-
-printf "\033[0;34mDeploy\033[0m | Compiling TypeScript...\n"
-npx tsc
-
-printf "\033[0;34mDeploy\033[0m | Starting the service...\n"
-npm start
